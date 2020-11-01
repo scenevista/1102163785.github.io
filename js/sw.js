ca4231e974e9461a71de5d4fac116a106206a537
李明臻 <ashquick2011@hotmail.com>
2020-11-02 00:06:23 +0800

    添加包

---
+++ js/sw.js
@@ -0,0 +1,14 @@
+﻿self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open("static-cache")
+            .then(function (cache) {
+                return cache.addAll([
+                    "/Content/bootstrap.css",
+                    "/Scripts/jquery-3.0.0.js",
+                    "/Scripts/bootstrap.js",
+                    "/Scripts/popper.js",
+                    "/favicon.png"
+                ]);
+            })
+    );
+});
